ae0a222f32c21c855f060a24ff6d7189aa354b29
jjmuchai <jjmuchai151@gmail.com>
2025-03-27 20:41:24 +0300

    My updates to the project

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left) || 0; 
+  if (left > 0) { 
+    dodger.style.left = `${left - 10}px`; 
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left) || 0; 
+  if (left < 360) { 
+    dodger.style.left = `${left + 10}px`; 
+  }
+}
